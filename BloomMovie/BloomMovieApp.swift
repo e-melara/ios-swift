76f8d9b8169f2190290b5b3c34470a45ba8333da
Edwin Melara <edwin.melara.landaverde@gmail.com>
2025-11-02 14:41:32 -0600

    Initial Commit

---
+++ BloomMovie/BloomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloomMovieApp.swift
+//  BloomMovie
+//
+//  Created by Edwin Melara on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BloomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
