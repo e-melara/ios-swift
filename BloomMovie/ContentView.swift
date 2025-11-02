a1d534c796feb58d840ab5f27b55bf2aa1757ea8
Edwin Melara <edwin.melara.landaverde@gmail.com>
2025-11-02 14:41:32 -0600

    Initial Commit

---
+++ BloomMovie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BloomMovie
+//
+//  Created by Edwin Melara on 2/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
